034c6de31ba46f13af5d3a764294d931cde7e66e
rkortlandt <rowankortlandt@otsegops.org>
2025-11-15 16:18:42 -0500

    fix

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // Set the base path to your repository name with leading and trailing slashes
+  base: '/FunnyClock/',
+  plugins: [],
+});
